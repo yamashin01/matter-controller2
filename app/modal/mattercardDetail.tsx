c8605374a5852cadc0fe474f8a121f849ecaf53f
yamada <shinya.yamada@benext-corp.com>
2024-05-23 11:05:26 +0900

    ✨ Set useForm

--- app/modal/mattercardDetail.tsx
+++ app/modal/mattercardDetail.tsx
@@ -1,25 +1,72 @@
-import { Modal, Button, Text } from "@mantine/core";
+import { Modal, Button, Group, TextInput, Checkbox } from "@mantine/core";
 import { MatterType } from "@/types/types";
+import { useForm } from "@mantine/form";
+import { useEffect } from "react";
 
 type Props = {
+  matterInfo: MatterType;
   opened: boolean;
   setOpened: any;
-  matterInfo: MatterType;
 };
 
 export function MatterCardDetailModal(props: Props) {
-  const matter = props.matterInfo;
+  const { matterInfo, opened } = props;
+  const form = useForm({
+    initialValues: {
+      id: "",
+      title: "",
+      created_at: "",
+      classification: "",
+      completed: false,
+      billing_amount: 0,
+      isFixed: false,
+      user_id: "",
+    },
+  });
+
+  useEffect(() => {
+    form.setValues({
+      id: matterInfo.id,
+      title: matterInfo.title,
+      created_at: matterInfo.created_at,
+      classification: matterInfo.classification,
+      completed: matterInfo.completed,
+      billing_amount: matterInfo.billing_amount,
+      isFixed: matterInfo.isFixed,
+      user_id: matterInfo.user_id,
+    });
+  }, [matterInfo]);
 
   const closeModal = () => {
     props.setOpened(false);
   };
-  return (
-    <>
-      <Modal opened={props.opened} onClose={closeModal} title="案件の詳細">
-        <Text>{matter.title}</Text>
-      </Modal>
 
-      {/* <Button onClick={open}>Open modal</Button> */}
-    </>
+  return (
+    <Modal opened={opened} onClose={closeModal} title={matterInfo.title}>
+      <form onSubmit={form.onSubmit((values) => console.log(values))}>
+        <TextInput
+          withAsterisk
+          label="案件名"
+          placeholder="案件名をご記入ください。"
+          {...form.getInputProps("title")}
+        />
+        <TextInput
+          withAsterisk
+          label="分類"
+          placeholder="案件の分類をご記入ください。"
+          {...form.getInputProps("classification")}
+        />
+
+        <Checkbox
+          mt="md"
+          label="確定"
+          {...form.getInputProps("isFixed", { type: "checkbox" })}
+        />
+
+        <Group justify="flex-end" mt="md">
+          <Button type="submit">Submit</Button>
+        </Group>
+      </form>
+    </Modal>
   );
 }
