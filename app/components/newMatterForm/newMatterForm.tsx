79c3d67b5dceac77adaa3f5a6b24c1b4b550775a
yamada <shinya.yamada@benext-corp.com>
2024-06-08 16:47:09 +0900

    ✨ Add input of cost info field to newMatterForm

--- app/components/newMatterForm/newMatterForm.tsx
+++ app/components/newMatterForm/newMatterForm.tsx
@@ -10,8 +10,13 @@ import {
   Title,
 } from "@mantine/core";
 import { useForm } from "@mantine/form";
+import { useState } from "react";
+import { CiSquarePlus } from "react-icons/ci";
+import { FaRegTrashAlt } from "react-icons/fa";
 
 const NewMatterForm = () => {
+  const [inputCountOfCostInfo, setInputCountOfCostInfo] = useState<number>();
+
   const form = useForm({
     mode: "uncontrolled",
     initialValues: {
@@ -61,16 +66,55 @@ const NewMatterForm = () => {
 
       <h1 className="pt-8 text-xl text-gray-700 font-bold">コスト</h1>
       <div className="pt-4">
-        <Group gap="sm" grow>
-          <h2 className="text-center">コスト名</h2>
-          <h2 className="text-center">品目</h2>
-          <h2 className="text-center">価格</h2>
-        </Group>
-        <Group gap="sm" grow>
-          <TextInput />
-          <TextInput />
-          <NumberInput />
-        </Group>
+        <div className="flex items-center">
+          <h2 className="flex-grow text-center">コスト名</h2>
+          <h2 className="flex-grow text-center">品目</h2>
+          <h2 className="flex-grow text-center">価格</h2>
+          <div className="w-10"></div>
+        </div>
+        <div className="flex items-center pb-2">
+          <Group gap="sm" className="flex-grow" grow>
+            <TextInput
+              placeholder="品名をご記入ください。"
+              className="flex-grow"
+            />
+            <Select
+              className="flex-grow"
+              placeholder="品目を選択ください。"
+              data={["システム料", "外注費", "備品購入"]}
+            />
+            <NumberInput placeholder="¥0" className="flex-grow" />
+          </Group>
+          <div className="h-full px-2 text-lg hover:cursor-pointer ml-auto flex items-center justify-center">
+            <FaRegTrashAlt />
+          </div>
+        </div>
+        <div className="flex items-center pb-2">
+          <Group gap="sm" className="flex-grow" grow>
+            <TextInput
+              placeholder="品名をご記入ください。"
+              className="flex-grow"
+            />
+            <Select
+              className="flex-grow"
+              placeholder="品目を選択ください。"
+              data={["システム料", "外注費", "備品購入"]}
+            />
+            <NumberInput placeholder="¥0" className="flex-grow" />
+          </Group>
+          <div className="h-full px-2 text-lg hover:cursor-pointer ml-auto flex items-center justify-center">
+            <FaRegTrashAlt />
+          </div>
+        </div>
+        <Button
+          type="button"
+          fullWidth
+          color="dark"
+          variant="outline"
+          rightSection={<CiSquarePlus />}
+        >
+          コスト追加
+        </Button>
       </div>
 
       <Group className="pt-8" justify="flex-end" mt="md">
