938df0fcc1171f40322dbb7eb082ab1ac2652d20
yamada <shinya.yamada@benext-corp.com>
2024-06-22 13:16:58 +0900

    ✨ Add error, not-found, loading pages

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,35 +1,13 @@
-// Import styles of packages that you've installed.
-// All packages except `@mantine/hooks` require styles imports
-import "@mantine/core/styles.css";
-import "../globals.css";
-
-import { ColorSchemeScript, MantineProvider } from "@mantine/core";
-import Header from "../components/header";
-import { Footer } from "../components/footer";
-import { Providers } from "../../components/providers";
-
-export const metadata = {
-  title: "案件管理システム",
-  description: "未来技術推進協会の案件を管理します。",
+const MainLayout = ({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) => {
+  return (
+    <div className="flex h-screen">
+      <main className=" bg-slate-50 flex-1 overflow-auto">{children}</main>
+    </div>
+  );
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="en">
-      <head>
-        <ColorSchemeScript />
-      </head>
-      <body>
-        <Providers>
-          <Header />
-          <MantineProvider>{children}</MantineProvider>
-          <Footer />
-        </Providers>
-      </body>
-    </html>
-  );
-}
+export default MainLayout;
